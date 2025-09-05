124daf2c6c92990fd2f8a78d98bb0af8e005af23
unknown <Vincius Rad>
2025-09-05 08:48:38 -0300

    feat: Adiciona o ResponseEntity no controller

--- src/main/java/com/livraria/biblioteca/controller/LivroController.java
+++ src/main/java/com/livraria/biblioteca/controller/LivroController.java
@@ -5,6 +5,8 @@ import com.livraria.biblioteca.mapper.LivroMapper;
 import com.livraria.biblioteca.model.LivroEntity;
 import com.livraria.biblioteca.repository.LivroRepository;
 import com.livraria.biblioteca.service.LivroService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,31 +29,32 @@ public class LivroController {
     }
 
     @PostMapping("/cadastrar")
-    public LivroEntity cadastrarLivro(@RequestBody LivroPatchDTO livroPatchDTO){
+    public ResponseEntity<LivroPatchDTO> cadastrarLivro(@RequestBody LivroPatchDTO livroPatchDTO){
         LivroEntity livroDto = livroMapper.toEntity(livroPatchDTO);
-        return livroService.cadastrarLivro(livroDto);
+        livroService.cadastrarLivro(livroDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroMapper.toDto(livroDto));
     }
     @GetMapping("/listar")
-    public List<LivroPatchDTO> listarLivro(){
-        return livroService.listarLivros()
+    public ResponseEntity<List<LivroPatchDTO>> listarLivro(){
+        return ResponseEntity.status(HttpStatus.FOUND).body(livroService.listarLivros()
                 .stream()
-                .map(livroMapper::toDto).collect(Collectors.toList());
+                .map(livroMapper::toDto).collect(Collectors.toList()));
     }
     @GetMapping("/listar/{id}")
-        public LivroPatchDTO listarLivroPorId(@PathVariable Long id){
+        public ResponseEntity<LivroPatchDTO> listarLivroPorId(@PathVariable Long id){
             LivroEntity entity = livroService.listarLivroPorId(id);
-            return livroMapper.toDto(entity);
+            return ResponseEntity.ok(livroMapper.toDto(entity));
         }
     @DeleteMapping("/deletar/{id}")
     public void deletarLivroPorId(@PathVariable Long id){
         livroService.deletarLivroPorId(id);
     }
     @PatchMapping("/atualizar/{id}")
-    public LivroPatchDTO atualizarLivro(@PathVariable Long id, @RequestBody LivroPatchDTO dto){
+    public ResponseEntity<LivroPatchDTO> atualizarLivro(@PathVariable Long id, @RequestBody LivroPatchDTO dto){
         LivroEntity livroBuscar = livroService.listarLivroPorId(id);
         dto.aplicarEm(livroBuscar);
         LivroEntity atualizado = livroService.cadastrarLivro(livroBuscar);
-        return livroMapper.toDto(atualizado);
+        return ResponseEntity.status(HttpStatus.CREATED).body(livroMapper.toDto(atualizado));
     }
 
 }
--- src/main/java/com/livraria/biblioteca/service/LivroService.java
+++ src/main/java/com/livraria/biblioteca/service/LivroService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class LivroService {
